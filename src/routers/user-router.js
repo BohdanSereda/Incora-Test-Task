3702ca11382a372c6594331a66ee485f0dae265d
BohdanSereda <zxysereda.bodik@gmail.com>
2022-07-21 23:11:24 +0300

    create user router

---
+++ src/routers/user-router.js
@@ -0,0 +1,19 @@
+const Router = require("express");
+const userController = require("../controllers/user-controller");
+const validationMiddleware = require("../middleware/validation");
+const router = new Router();
+
+router.post(
+    "/users",
+    validationMiddleware.validateUser,
+    userController.createUser
+);
+router.get("/users/:id", userController.getUser);
+router.post("/login");
+router.put(
+    "/users/:id",
+    validationMiddleware.validateUserUpdates,
+    userController.updateUser
+);
+
+module.exports = router;
