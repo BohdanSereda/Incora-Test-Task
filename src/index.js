da14e2ed2786d6e1e8754053e63f56d4551d55b8
BohdanSereda <zxysereda.bodik@gmail.com>
2022-07-22 01:34:33 +0300

    create push notification event on user update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
 const sequelize = require("./database/db");
 const userRouter = require("./routers/user-router");
-const userModel = require("./models/user-model");
 const errorMiddleware = require("./middleware/error");
 const port = process.env.PORT || 3000;
 const app = express();
@@ -10,11 +11,22 @@ const app = express();
 app.use(express.json());
 app.use(userRouter);
 app.use(errorMiddleware);
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+    socket.on("updateUser", () => {
+        socket.emit("pushNotification", user);
+    });
+});
 const start = async () => {
     try {
         await sequelize.authenticate();
         await sequelize.sync();
-        app.listen(port, () => console.log(`Server started on PORT ${port}`));
+        server.listen(port, () =>
+            console.log(`Server started on PORT ${port}`)
+        );
     } catch (err) {
         console.log(err);
     }
