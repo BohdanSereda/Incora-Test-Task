cf4554a69abe8a0d51cfb059df1cfd2c0c953765
BohdanSereda <zxysereda.bodik@gmail.com>
2022-07-21 23:09:00 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const sequelize = require("./database/db");
+const userRouter = require("./routers/user-router");
+const userModel = require("./models/user-model");
+const errorMiddleware = require("./middleware/error");
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(userRouter);
-app.use(taskRouter);
 app.use(errorMiddleware);
-
 const start = async () => {
     try {
-        app.listen(port, () => console.log(`Server started on PORT = ${port}`));
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(port, () => console.log(`Server started on PORT ${port}`));
     } catch (err) {
         console.log(err);
     }
